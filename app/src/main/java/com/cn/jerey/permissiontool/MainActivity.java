a126db8a8dd59cdd1238d0762f9bb3ceb7a9af55
Xiamin <610315802@qq.com>
2017-01-24 22:28:11 +0800

    增加申请权限类使用demo

--- app/src/main/java/com/cn/jerey/permissiontool/MainActivity.java
+++ app/src/main/java/com/cn/jerey/permissiontool/MainActivity.java
@@ -1,13 +1,73 @@
 package com.cn.jerey.permissiontool;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.cn.jerey.permissiontools.Callback.PermissionCallbacks;
+import com.cn.jerey.permissiontools.PermissionTools;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    PermissionTools permissionTools;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                permissionTools =  new PermissionTools.Builder(this)
+                        .setOnPermissionCallbacks(new PermissionCallbacks() {
+                            @Override
+                            public void onPermissionsGranted(int requestCode, List<String> perms) {
+                                Toast.makeText(MainActivity.this,"权限申请通过",Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onPermissionsDenied(int requestCode, List<String> perms) {
+                                Toast.makeText(MainActivity.this,"权限申请被拒绝",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setRequestCode(111)
+                        .build();
+                permissionTools.requestPermissions(Manifest.permission.CAMERA);
+
+                break;
+            case R.id.button2:
+                permissionTools =  new PermissionTools.Builder(this)
+                        .setOnPermissionCallbacks(new PermissionCallbacks() {
+                            @Override
+                            public void onPermissionsGranted(int requestCode, List<String> perms) {
+                                Toast.makeText(MainActivity.this,"权限申请通过" + perms,Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onPermissionsDenied(int requestCode, List<String> perms) {
+                                Toast.makeText(MainActivity.this,"权限申请被拒绝" + perms,Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setRequestCode(111)
+                        .build();
+                permissionTools.requestPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionTools.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 }
